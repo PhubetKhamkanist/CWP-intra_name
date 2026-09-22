bbf7cef574237250e9e04a1c7494ae5efcacc61f
PhubetKhamkanist <khamkanistphubet@gmail.com>
2026-09-22 12:09:35 +0000

    Add main functions for number input and loop in to25.py and multiplicatoin_table

---
+++ discovery_piscine/cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+def main():
+    num = int(input('Enter a number less than 25\n'))
+    
+    if num > 25:
+        print('Error')
+    else:
+        while num <= 25:
+            print(f'Inside the loop, my variable is {num}')
+            num += 1
+
+main()

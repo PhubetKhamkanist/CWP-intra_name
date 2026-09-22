2212a064d639ddb72795ff5a6c8f857f33c6bbf6
PhubetKhamkanist <khamkanistphubet@gmail.com>
2026-09-22 04:11:01 +0000

    cell 1.1-1.3

---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = 'Phubet'
+last_name = 'Khamkanist'
+whole_name = first_name + ' ' + last_name
+print(f'{whole_name}')
---
+++ discovery_piscine/cell01/ex02/age.py
@@ -0,0 +1,3 @@
+age = int(input())
+my_age = age + 42
+print(my_age)

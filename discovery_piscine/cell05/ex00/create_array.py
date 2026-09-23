034f25559a3470dbecb9856476c2288911ad8513
phubet.kha <ext.phubet.kha@truedigital.com>
2026-09-23 13:47:36 +0700

    Add create_array.py and play_with_arrays.py with main functions for array manipulation

---
+++ discovery_piscine/cell05/ex00/create_array.py
@@ -0,0 +1,7 @@
+""" create_array.py """
+def main():
+    """ create_array.py """
+    lst = [2, 8, 9, 48, 8, 22, -12, 2]
+    print(lst)
+
+main()
---
+++ discovery_piscine/cell05/ex02/play_with_arrays.py
@@ -0,0 +1,9 @@
+""" play_with_arrays.py """
+def main():
+    """ play_with_arrays.py """
+    lst = [2, 8, 9, 48, 8, 22, -12, 2]
+    new_lst = [i+2 for i in lst if i+2 > 5]
+    print(lst)
+    print(new_lst)
+
+main()
